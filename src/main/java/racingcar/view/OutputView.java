983ffbb25387cf4d65636d4fabb1915235de739e
momnpa333 <momnpa333@gmail.com>
2025-10-26 15:50:53 +0900

    refactor(OutputView): static final로 상수 관리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,23 +1,28 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
 public class OutputView {
 
-    public void printResult(String result) {
-        System.out.println(result);
-    }
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String RESULT_SEPARATOR = " : ";
+    private static final String PROGRESS_MARK = "-";
 
     public void printRoundResult(List<Car> roundResult) {
         for (Car car : roundResult) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            System.out.println(
+                car.getName() + RESULT_SEPARATOR + PROGRESS_MARK.repeat(car.getPosition()));
         }
         System.out.println();
     }
 
-    public void printFinalWinners(String winners) {
-        System.out.println("최종 우승자: " + winners);
+    public void printWinners(List<Car> winners) {
+        String names = winners.stream()
+            .map(Car::getName)
+            .collect(Collectors.joining(", "));
+        System.out.println(FINAL_WINNER_MESSAGE + names);
     }
 
 
