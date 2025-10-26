dbbf784a7a418dfad8eec3ce4525008586efff13
momnpa333 <momnpa333@gmail.com>
2025-10-26 14:53:30 +0900

    feat(Car): move 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int optionNumber) {
+        if (optionNumber >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+}
