344a996e2e6f7897fc6a78cff3c2f01e4e0c8f43
momnpa333 <momnpa333@gmail.com>
2025-10-26 14:14:20 +0900

    feat(GameController): GameController 스켈레톤 코드 제작

---
+++ src/main/java/racingcar/domain/GameController.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class GameController {
+
+    private final RacingGame racingGame;
+
+    public GameController(RacingGame racingGame) {
+        this.racingGame = racingGame;
+    }
+
+    public void run() {
+        // 게임 시작
+    }
+}
