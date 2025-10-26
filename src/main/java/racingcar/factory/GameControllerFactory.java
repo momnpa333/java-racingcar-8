db1d358fd353a6c27b9e4f27b6ca56ec36cb125e
momnpa333 <momnpa333@gmail.com>
2025-10-26 16:32:30 +0900

    refactor(GameControllerFactory): GameControllerFactory 제작

---
+++ src/main/java/racingcar/factory/GameControllerFactory.java
@@ -0,0 +1,44 @@
+package racingcar.factory;
+
+import racingcar.domain.GameController;
+import racingcar.domain.Parser;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomMoveStrategy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.Viewer;
+
+public final class GameControllerFactory {
+
+    private GameControllerFactory() {
+    }
+
+    public static GameController gameController() {
+        return new GameController(racingGame());
+    }
+
+    private static RacingGame racingGame() {
+        return new RacingGame(viewer(), parser(), randomMoveStrategy());
+    }
+
+    private static Viewer viewer() {
+        return new Viewer(outputView(), inputView());
+    }
+
+    private static Parser parser() {
+        return new Parser();
+    }
+
+    private static RandomMoveStrategy randomMoveStrategy() {
+        return new RandomMoveStrategy();
+    }
+
+    private static InputView inputView() {
+        return new InputView();
+    }
+
+    private static OutputView outputView() {
+        return new OutputView();
+    }
+}
+
