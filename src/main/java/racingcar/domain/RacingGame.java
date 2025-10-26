29e522db8ad9a98f1f295a0811a09498e5b58ce2
momnpa333 <momnpa333@gmail.com>
2025-10-26 15:48:40 +0900

    refactor(RacingGame): moveCar와 determineWinners 책임 CarGroup에게 위임

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.view.Viewer;
 
@@ -8,64 +7,37 @@ public class RacingGame {
 
     private final Viewer viewer;
     private final Parser parser;
-    private List<Car> cars;
+    private final MoveStrategy moveStrategy;
+    private CarGroup carGroup;
 
-    public RacingGame(Viewer viewer, Parser parser) {
+
+    public RacingGame(Viewer viewer, Parser parser, MoveStrategy moveStrategy) {
         this.viewer = viewer;
         this.parser = parser;
+        this.moveStrategy = moveStrategy;
     }
 
     public void start() {
-        // 자동차 이름 입력 및 자동차 초기화
-        initializeCars(viewer.readCarNames());
-        // 자동차 게임 횟수 입력
+        initializeCarGroup(viewer.readCarNames());
         int rounds = Integer.parseInt(viewer.readTryCount());
-        // 게임 실행
         playGame(rounds);
-        // 최종 우승자 출력
-        String winners = determineWinners();
-        viewer.printFinalWinners(winners);
-
+        viewer.printWinners(carGroup.getWinners());
     }
 
-    private void initializeCars(String input) {
-        // 자동차 이름을 기반으로 Car 객체 생성 및 cars 리스트 초기화
+    // 입력된 자동차 이름을 파싱하여 자동차 객체 생성 및 CarGroup 초기화
+    private void initializeCarGroup(String input) {
         List<String> carNames = parser.parseCarNames(input);
-        cars = carNames.stream()
+        List<Car> cars = carNames.stream()
             .map(Car::new)
             .toList();
+        this.carGroup = new CarGroup(cars);
     }
 
     private void playGame(int rounds) {
-        // 주어진 횟수만큼 게임을 진행
         for (int i = 0; i < rounds; i++) {
-            // 각 차수별로 모든 자동차의 위치 업데이트
-            updateCarPositions();
-            // 차수별 결과 출력
-            viewer.printRoundResult(cars);
+            carGroup.moveCars(moveStrategy);
+            viewer.printRoundResult(carGroup.getCars());
         }
-
-    }
-
-    private void updateCarPositions() {
-        for (Car car : cars) {
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
-            car.move(randomNumber);
-        }
-    }
-
-    private String determineWinners() {
-        int maxPosition = cars.stream()
-            .mapToInt(Car::getPosition)
-            .max()
-            .orElse(0);
-
-        List<String> winners = cars.stream()
-            .filter(car -> car.getPosition() == maxPosition)
-            .map(Car::getName)
-            .toList();
-
-        return String.join(", ", winners);
     }
 
 }
