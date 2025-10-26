9fb241ded84273f1b7faae47f63bfac38f82654e
momnpa333 <momnpa333@gmail.com>
2025-10-26 16:33:52 +0900

    refactor(Application): DI 컨테이너 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,24 +1,12 @@
 package racingcar;
 
 import racingcar.domain.GameController;
-import racingcar.domain.Parser;
-import racingcar.domain.RacingGame;
-import racingcar.domain.RandomMoveStrategy;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
-import racingcar.view.Viewer;
+import racingcar.factory.GameControllerFactory;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        Viewer viewer = new Viewer(outputView, inputView);
-        Parser parser = new Parser();
-        RandomMoveStrategy randomMoveStrategy = new RandomMoveStrategy();
-        RacingGame racingGame = new RacingGame(viewer, parser, randomMoveStrategy);
-        GameController gameController = new GameController(racingGame);
+        GameController gameController = GameControllerFactory.gameController();
         gameController.run();
     }
 }
